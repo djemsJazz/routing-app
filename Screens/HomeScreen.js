279d61c06077ed1df9c802b1acca1417a2a8de78
djemsJazz <dahmdjamel92@gmail.com>
2021-02-05 17:16:32 +0100

    Adding tab navigator in Home screen

--- Screens/HomeScreen.js
+++ Screens/HomeScreen.js
@@ -1,8 +1,50 @@
 import React from 'react'
-import { View, Text, Button } from 'react-native'
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import FeedTadScreen from './FeedTadScreen';
+import MessagesTadScreen from './MessagesTadScreen';
+import { Ionicons } from '@expo/vector-icons';
 
-const HomeScreen = ({ navigation }) => (
-  <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+const Tab = createBottomTabNavigator();
+
+const getTabIconName = (routeName, focused) => {
+  let iconName = '';
+  if (routeName === 'feeds') {
+    iconName = focused ? 'mail-open' : 'mail-open-outline';
+  } else if (routeName === 'messages') {
+    iconName = focused ? 'ios-list-circle' : 'ios-list';
+  }
+  return iconName
+}
+
+const HomeScreen = () => (
+  <Tab.Navigator
+    screenOptions={({ route }) => ({
+      tabBarIcon: ({ focused, color, size }) => {
+        const iconName = getTabIconName(route.name, focused);
+        return <Ionicons name={iconName} size={size} color={color} />;
+      },
+    })}
+    tabBarOptions={{
+      activeTintColor: 'tomato',
+      inactiveTintColor: 'gray',
+      labelStyle: {
+        fontSize: 13,
+        marginTop: -5
+      },
+      style: {
+        paddingBottom: 2,
+      }
+    }}
+  >
+    <Tab.Screen name="feeds" component={FeedTadScreen} options={{ title: 'Feeds', }} />
+    <Tab.Screen name="messages" component={MessagesTadScreen} options={{ title: 'Messages', tabBarBadge: 5 }} />
+  </Tab.Navigator>
+);
+
+export default HomeScreen;
+
+
+{/* <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
     <Text>Home screen</Text>
     <Button
       title="Go to Details"
@@ -15,7 +57,4 @@ const HomeScreen = ({ navigation }) => (
       title="Go to Profile"
       onPress={() => navigation.navigate('profile')}
     />
-  </View>
-);
-
-export default HomeScreen;
+  </View> */}
