561b200c7adece53b970a6b4e39cc8c6f2305e3c
djemsJazz <dahmdjamel92@gmail.com>
2021-02-22 19:23:01 +0100

    Renaming Rting folder to Navigation

--- Navigation/AppRouter.js
+++ Navigation/AppRouter.js
@@ -1,32 +1,32 @@
 import React from 'react'
-import { NavigationContainer } from '@react-navigation/native';
+import { useNavigation } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack'
 import HomeScreen from '../Screens/HomeScreen';
 import DetailsScreen from '../Screens/DetailsScreen';
 import ProfileScreen from '../Screens/ProfileScreen';
-import {StatusBar} from 'expo-status-bar';
 import { Ionicons } from '@expo/vector-icons';
 
 const Stack = createStackNavigator();
-const screensOptions = {
-  headerStyle: { backgroundColor: 'tomato' },
-  headerTintColor: '#fff',
-  headerTitleStyle: {
-    alignSelf: 'center',
-  },
-  headerRight: () => (
-    <Ionicons
-      onPress={() => alert('This is a button!')}
-      name="menu"
-      color="#fff"
-      size={45}
-    />
-  ),
-};
 
-const AppRouter = () => (
-  <NavigationContainer>
-    <StatusBar style="light" backgroundColor="#da563d" />
+
+const AppRouter = () => {
+  const navigation = useNavigation();
+  const screensOptions = {
+    headerStyle: { backgroundColor: 'tomato' },
+    headerTintColor: '#fff',
+    headerTitleStyle: {
+      alignSelf: 'center',
+    },
+    headerRight: () => (
+      <Ionicons
+        onPress={() => navigation.toggleDrawer()}
+        name="menu"
+        color="#fff"
+        size={45}
+      />
+    ),
+  };
+  return (
     <Stack.Navigator initialRouteName="home" screenOptions={screensOptions}>
       <Stack.Screen name="home" component={HomeScreen} options={{ title: null }} />
       <Stack.Screen name="details" options={{ title: "Details screen" }}>
@@ -38,7 +38,7 @@ const AppRouter = () => (
         options={({ route }) => ({ title: (route.params && route.params.name) ? route.params.name : "Profile Screen" })}
       />
     </Stack.Navigator>
-  </NavigationContainer>
-);
+  );
+}
 
 export default AppRouter
